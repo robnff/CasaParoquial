5063eef54bfb71daff47a535b2ea4d558b20135f
Rayana Alencar <rvsa2@cin.ufpe.br>
2016-06-14 22:55:47 -0300

    Add files via upload

--- casaParoquial/urls.py
+++ casaParoquial/urls.py
@@ -21,12 +21,19 @@ urlpatterns = [
     url(r'^cadastrar_evento/$', views.cadastrar_evento),
     url(r'^cadastrar_membro/$', views.cadastrar_membro),
     url(r'^consultar_membro/$', views.consulta_membro),
+    url(r'^membro/(?P<membro_id>\d+)/$', views.editar_membro),
+    url(r'^evento/(?P<evento_id>\d+)/$', views.editar_evento),
     url(r'^consultar_evento/$', views.consultar_evento),
     url(r'^consultar_aconselhamento/$', views.consultar_aconselhamento),
     url(r'^cadastrar_aconselhamento/$', views.cadastrar_aconselhamento),
+    url(r'^relatorio_aniversario/$', views.relatorio_aniversario),
+    url(r'^consultar_evento_nome/$', views.consultar_evento_nome),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^email/$', views.envia_email),
     url(r'^criandouser1/$', views.login_user1),
     url(r'^criandouser2/$', views.login_user2),
-
+    url(r'^deslog/$', views.deslog),
+    url(r'^relatorio_ministerio/$', views.relatorio_ministerio),
+    url(r'^relatorio_demografico/$', views.relatorio_demografico),
+   
 ]
